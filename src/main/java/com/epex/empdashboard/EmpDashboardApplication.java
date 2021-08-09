a06ab75d8a08f05dda24cf5946f622b8902f589f
Rajanish <rajanishatwork@gmail.com>
2021-08-10 03:09:05 +0530

    initial commit
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

---
+++ src/main/java/com/epex/empdashboard/EmpDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.epex.empdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/epex/empdashboard/EmpDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epex.empdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
