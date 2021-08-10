7320aeb6ec2b641a5509f997a7bdb2574cff1d25
Rajanish <rajanishatwork@gmail.com>
2021-08-11 05:02:30 +0530

    request intercepting with zuul added
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

--- src/main/java/com/epex/empdashboard/EmpDashboardApplication.java
+++ src/main/java/com/epex/empdashboard/EmpDashboardApplication.java
@@ -1,13 +1,21 @@
 package com.epex.empdashboard;
 
+import com.epex.empdashboard.filters.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class EmpDashboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmpDashboardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmpDashboardApplication.class, args);
+    }
 
+    @Bean
+    public SimpleFilter simpleFilter() {
+        return new SimpleFilter();
+    }
 }
