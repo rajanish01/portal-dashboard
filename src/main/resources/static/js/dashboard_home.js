9db82deb5d98162b25177d7d5f181b925e7e663f
Rajanish <rajanishatwork@gmail.com>
2021-09-15 05:18:05 +0530

    ui changes for modal and nav
    Signed-off-by: Rajanish <rajanishatwork@gmail.com>

--- src/main/resources/static/js/dashboard_home.js
+++ src/main/resources/static/js/dashboard_home.js
@@ -1,10 +1,6 @@
-$(document).ready(function () {
-  $("#login_btn").click(function () {
-    $('[data-toggle="tooltip"]').tooltip()
-  });
-
-  $("#login_btn_submit").click(function () {
-    login();
-  });
+$(document).ready(function() {
+    $("#login_btn_submit").click(function() {
+        login();
+    });
 
 });
--- src/main/resources/static/js/http_request_handler.js
+++ src/main/resources/static/js/http_request_handler.js
@@ -1,46 +1,53 @@
-
 function login() {
-  var username = $("#username1").val();
-  var password = $("#password1").val();
-  var data = {
-    "username": username,
-    "password": password
-  }
-  post("./authenticate", data);
+    var username = $("#username1").val();
+    var password = $("#password1").val();
+    var data = {
+        "username": username,
+        "password": password
+    }
+    post("./authenticate", data);
 }
 
 function post(url, data) {
-  $.ajax({
-    contentType: 'application/json',
-    data: JSON.stringify(data),
-    dataType: 'json',
-    success: function (response) {
-      console.log(response);
-      $('[data-toggle="tooltip"]').tooltip();
-      return response;
-    },
-    error: function () {
-      console.log("Login Failed !");
-      alert("Login Failed !")
-      return null;
-    },
-    processData: false,
-    type: 'POST',
-    url: url
-  });
+    $.ajax({
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        dataType: 'json',
+        success: function(response) {
+            console.log(response);
+            $("#loginModal").modal('toggle')
+            success("User Logged In !")
+            return response;
+        },
+        error: function(error) {
+            console.log(formatError(error));
+            danger(formatError(error));
+        },
+        processData: false,
+        type: 'POST',
+        url: url
+    });
 }
 
 function get(url) {
-  $.get(url, function () {
-    alert("success");
-  })
-    .done(function () {
-      alert("second success");
-    })
-    .fail(function () {
-      alert("error");
-    })
-    .always(function () {
-      alert("finished");
-    }); l̥
+    $.get(url, function() {
+            alert("success");
+        })
+        .done(function() {
+            alert("second success");
+        })
+        .fail(function() {
+            alert("error");
+        })
+        .always(function() {
+            alert("finished");
+        });
+}
+
+function formatError(error) {
+    var errorCode = error.status;
+    var errorMessage = error.responseJSON.message;
+    var detailError = error.responseJSON.details[0]
+
+    return errorMessage + " : " + errorCode + " -> " + detailError;
 }
